68476b28dff227fadafd122ba7a54a3bbc421d5e
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-13 13:21:37 +0530

    input box and search icon added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-
+import searchIcon from "./assets/search.png";
 function App() {
   return (
-    <div className="conatainer">
+    <div className="container">
       <div className="inner-container">
-        <input type="text" placeholder="Search city" className=""/>
+        <input type="text" placeholder="Search city" className="city-search"/>
+        <div className="search-bar">
+        <img src={searchIcon} alt="" width="30px" height="30px"/>
+        </div>
       </div>
     </div>
   );
