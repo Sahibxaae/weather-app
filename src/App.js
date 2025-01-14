868b57f8aaf393a1118e38f93e6098b5a8333f47
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-14 18:39:09 +0530

    all styles are added

--- src/App.js
+++ src/App.js
@@ -9,16 +9,38 @@ import rain from "./assets/rain.png";
 import snow from "./assets/snow.png";
 import wind from "./assets/wind (1).png";
 import humidity from "./assets/humidity.png";
+import { useState } from "react";
 function App() {
+  const [icon, setIcon] = useState(clearIcon);
+  const [temp, setTemp] = useState(38);
+  const [condition, setCondition] = useState("clear");
+  const [city, setCity] = useState("Chennai");
+  const [country, setCountry] = useState("india");
+  const [long, setLong] = useState(0);
+  const [lat, setLat] = useState(0);
+  const [humid, setHumid] = useState(0);
+  const [windSpeed, setWindSpeed] = useState(0);
   return (
     <div className="container">
       <div className="inner-container">
-        <input type="text" placeholder="Search city" className="city-search"/>
+        <input type="text" placeholder="Search city" className="city-search" />
         <div className="search-bar">
-        <img src={searchIcon} alt="" width="30px" height="30px"/>
+          <img src={searchIcon} alt="" width="30px" height="30px" />
         </div>
       </div>
-      <WeatherDetails clrImage={clearIcon}/>
+      <WeatherDetails
+        clrImage={icon}
+        temperature={temp}
+        city={city}
+        country={country}
+        condition={condition}
+        long={long}
+        lat={lat}
+        humidity={humidity}
+        humid={humid}
+        wind={wind}
+        windSpeed={windSpeed}
+      />
     </div>
   );
 }
--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const WeatherDetails = (props) => {
   return (
-    <div className='image'>
+    <div>
+      <div className="image">
         <img src={props.clrImage} alt="" />
-        <p>Sunny</p>
+        <div className="temp">{props.temperature} °C</div>
+        <div className="city">{props.city}</div>
+        <div className="country">{props.country}</div>
+        <div className="condition">{props.condition}</div>
+        <div className="long-lat">
+          <span>Longitute:{props.long}</span> 
+          <span>Latitude:{props.lat}</span>
+        </div>
+      </div>
+      <div className="humid-wind">
+          <div className="humid"> 
+            <img src={props.humidity} alt="Humidity"/>
+            <p>{props.humid}%</p>
+          </div>
+          <div className="wind">
+            <img src={props.wind} alt="wind speed"/>
+            <p>{props.windSpeed} Kmph</p>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherDetails
+export default WeatherDetails;
